2b238ad3b7a59ac63f99e35dfc4977399d53c18f
saima-riaz <saimariaz1422@gmail.com>
2024-04-21 21:54:16 +0200

    ordering listing

--- markdown2html.py
+++ markdown2html.py
@@ -1,39 +1,51 @@
 #!/usr/bin/python3
-"""
-Write a script markdown2html.py that takes two string arguments:
-
-    First argument is the name of the Markdown file
-    Second argument is the output file name
-"""
 
 import sys
-import os.path
-import re
-import hashlib
 
 def markdown_to_html(md_file, html_file):
+    """
+    Convert Markdown syntax to HTML syntax for headings and lists.
+
+    Args:
+        md_file (str): Path to the input Markdown file.
+        html_file (str): Path to the output HTML file.
+    """
     with open(md_file, 'r') as md:
         markdown_content = md.readlines()
 
     html_content = ""
     in_list = False
+    is_ordered_list = False
 
     for line in markdown_content:
         if line.startswith("#"):
             html_content += f"<h1>{line.strip('#').strip()}</h1>"
-        elif line.startswith("-"):
-            if not in_list:
+        elif line.startswith("*"):
+            if not in_list or is_ordered_list:
                 html_content += "<ul>"
                 in_list = True
-            html_content += f"<li>{line.strip('-').strip()}</li>"
+                is_ordered_list = False
+            html_content += f"<li>{line.strip('*').strip()}</li>"
+        elif line.startswith("1."):
+            if not in_list or not is_ordered_list:
+                html_content += "<ol>"
+                in_list = True
+                is_ordered_list = True
+            html_content += f"<li>{line.strip('1.').strip()}</li>"
         else:
             if in_list:
-                html_content += "</ul>"
+                if is_ordered_list:
+                    html_content += "</ol>"
+                else:
+                    html_content += "</ul>"
                 in_list = False
             html_content += line
 
     if in_list:
-        html_content += "</ul>"
+        if is_ordered_list:
+            html_content += "</ol>"
+        else:
+            html_content += "</ul>"
 
     with open(html_file, 'w') as html:
         html.write(html_content)
